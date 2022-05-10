3ebfcc6c078158c95b5d8f3ed128cd4db24b7203
SavioAraujoPagung <savio.pagung@hotmail.com>
2022-05-10 19:49:16 -0300

    feat: list products

--- dash/src/app/app-routing.module.ts
+++ dash/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CadastroUsuarioComponent } from './pages/cadastro-usuario/cadastro-usuario.component';
+import { BuscarProdutoComponent } from './pages/buscar-produto/buscar-produto.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path:'', pathMatch: 'full' ,redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
-  { path: 'dash', component: DashboardComponent }
+  { path: 'dash', component: DashboardComponent },
+  { path: 'buscar-produtos', component: BuscarProdutoComponent }
 ];
 
 @NgModule({
