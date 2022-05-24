20f6721f192d8ae7363ad6e87144f10bec20b84e
Julia Barcellos <barcellosjum@gmail.com>
2022-05-24 20:09:43 -0300

    Telas novas de fiscal saída e entrada

--- dash/src/app/app-routing.module.ts
+++ dash/src/app/app-routing.module.ts
@@ -4,8 +4,12 @@ import { BuscarProdutoComponent } from './pages/buscar-produto/buscar-produto.co
 import { CadastroProdutoComponent } from './pages/cadastro-produto/cadastro-produto.component';
 import { CadastroUsuarioComponent } from './pages/cadastro-usuario/cadastro-usuario.component';
 import { CafeteriaComponent } from './pages/cafeteria/cafeteria.component';
+import { CaixaComponent } from './pages/caixa/caixa.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FiscalEntradaComponent } from './pages/fiscal-entrada/fiscal-entrada.component';
+import { FiscalSaidaComponent } from './pages/fiscal-saida/fiscal-saida.component';
 import { LoginComponent } from './pages/login/login.component';
+
 
 const routes: Routes = [
   { path:'', pathMatch: 'full' ,redirectTo: 'login' },
@@ -14,7 +18,11 @@ const routes: Routes = [
   { path: 'buscar-produtos', component: BuscarProdutoComponent },
   { path: 'cadastrar-usuario', component:  CadastroUsuarioComponent },
   { path: 'cadastrar-produtos', component:  CadastroProdutoComponent },
-  { path: 'cafeteria', component:  CafeteriaComponent }
+  { path: 'cafeteria', component:  CafeteriaComponent },
+  { path: 'fiscal-entrada', component:  FiscalEntradaComponent },
+  { path: 'fiscal-saida', component:  FiscalSaidaComponent },
+  { path: 'caixa', component:  CaixaComponent }
+
 ];
 
 @NgModule({
